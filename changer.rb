65c91a799b3914b902dbf04ef30543715db47981
Abby Christens <achristens@gmail.com>
2017-11-06 18:17:18 -0500

    Find complex change

--- changer.rb
+++ changer.rb
@@ -1,13 +1,17 @@
 class Changer
 
-  def self.make_change(number)
-    change = []
-
-    if number % 2 == 0 && number > 10
-      change += [number / 2] * 2
-    else
-      change << number
+  def self.make_change(amount)
+  type_of_coins    = [25,10,5,1]
+  coins            = []
+  leftover_amount  = amount
+  type_of_coins.each do |coin|
+    if ((leftover_amount/coin) > 0)
+      (leftover_amount/coin).times { coins << coin }
+      leftover_amount = amount - coins.reduce(:+)
     end
   end
 
+  return coins
+
+  end
 end
